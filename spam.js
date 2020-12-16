bcf9eed261366d948a381e65c73787ee94d5a66f
salamah <>
2020-12-16 08:34:54 -0800

    first commit

---
+++ spam.js
@@ -0,0 +1,10 @@
+/*for (let i=100; i<100; i++){
+    console.log('Spam alert')
+}*/
+for(let i=0; i<=100; i++){
+    console.log(i);
+    //document.write('This is a spam')
+   // (s.split('')); //split
+    console.log('This is a spam')
+}
+const name=console.log('salamah')
