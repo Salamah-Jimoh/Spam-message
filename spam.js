777f67b46368f9b2a3474e4cbf8fb87a6d247ce7
salamah <salamahjimoh@gmail.com>
2020-12-17 12:01:18 -0800

    modified spam.js

--- spam.js
+++ spam.js
@@ -1,10 +1,9 @@
 /*for (let i=100; i<100; i++){
     console.log('Spam alert')
 }*/
-for(let i=0; i<=100; i++){
+for(let i=0; i<=1000; i++){
     console.log(i);
     //document.write('This is a spam')
    // (s.split('')); //split
     console.log('This is a spam')
 }
-const name=console.log('salamah')
